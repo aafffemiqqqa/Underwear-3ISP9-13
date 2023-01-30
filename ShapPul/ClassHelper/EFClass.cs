8fb3ca9ae072fcc680684bdb208d9faff912fbb4
Kamilla <kamilla.kumysheva@mail.ru>
2023-01-30 11:01:03 +0300

    Добавьте файлы проекта.

---
+++ ShapPul/ClassHelper/EFClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapPul.DB;
+
+namespace ShapPul.ClassHelper
+{
+    class EFClass
+    {
+        public static DB.Entities1 Context { get; } = new DB.Entities1();
+    }
+}
---
+++ ShapPul/Windows/Autorisation.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static ShapPul.ClassHelper.EFClass;
+
+namespace ShapPul.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorisation.xaml
+    /// </summary>
+    public partial class Autorisation : Window
+    {
+        public Autorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Enter_Click(object sender, RoutedEventArgs e)
+        {
+            var userAuth = Context.Account.ToList()
+                .Where(i => i.Login == TbLog.Text && i.Password == TbPsw.Text)
+                .FirstOrDefault();
+            if (userAuth != null)
+            {
+                MessageBox.Show("ОК");
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
